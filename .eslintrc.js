c11e1cf58444490c2b4316693f7120e05c23f450
Yuji Ueki <unhappychoice@gmail.com>
2020-09-03 23:35:02 +0900

    :shirt: Add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": [
+        "standard"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+      "no-useless-constructor": 0,
+    }
+};
