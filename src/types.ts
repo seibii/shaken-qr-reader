b8867e612cc80c272844f288b3aa670aa4f0c253
amatotsuji <amatoten@gmail.com>
2021-02-02 11:13:40 +0900

    :+1: `instanceof` can be worked for error customizing classes

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,16 @@
 export type CarType = 'normal' | 'kei' | 'kei_old'
 
-export class InvalidQRCode extends Error {}
-export class ReadQRCode extends Error {}
+export class InvalidQRCode extends Error {
+  constructor (e?: string) {
+    super(e)
+    this.name = new.target.name
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
+}
+export class ReadQRCode extends Error {
+  constructor (e?: string) {
+    super(e)
+    this.name = new.target.name
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
+}
